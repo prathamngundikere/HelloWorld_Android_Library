a07225a5ff4db34dfc36e942c952cd9ec1539154
PRATHAM N GUNDIKERE <prathamgundikere@gmail.com>
2025-03-14 12:49:21 +0530

    Implementation of library

--- app/src/main/java/io/github/prathamngundikere/helloworld/MainActivity.kt
+++ app/src/main/java/io/github/prathamngundikere/helloworld/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.prathamngundikere.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +17,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    HelloWorld(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloWorldTheme {
-        Greeting("Android")
     }
 }
